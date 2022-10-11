9949ba479036ed68e327db7721c5bc03620e1c93
njoro <njorogeamos6@gmail.com>
2022-10-11 20:13:28 +0300

    added voice reader library

--- demo/app.py
+++ demo/app.py
@@ -1,34 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+import pyPDF2
+import pyttsx3
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5e979b170e38298397c45bb514d7a5fab744166fb5834ab4'
 
-@app.route("/")
+@app.route("/", methods=('GET', 'POST'))
 def hello_world():
+    toRead = []
+    if request.method == 'POST':
+        title = request.form['inputPassword']
+        toRead.append[title]
+    else:
+        return redirect(url_for('home'))
+
+    for va in toRead:
+        pdfobj = open(va, 'rb')
+
+        pdfreader = pyPDF2.PdfFileReader(pdfobj)
+
+        text = ""
+
+        for pagenumber in range(pdfreader.numPages):
+            page = pdfreader.getPage(pagenumber)
+            text += page.extractText()
+
+        pdfobj.close()
+
+        print(text)
+
+        engine = pyttsx3.init()
+        engine.say(text)
+        engine.runAndWait()
+        
+    
     return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-'''import pyPDF2
-import pyttsx3
+'''
 
 file_to_read = input("Please enter file name:")
 
- pdfobj = open(file_to_read, 'rb')
-
- pdfreader = pyPDF2.PdfFileReader(pdfobj)
-
- text = ""
-
- for pagenumber in range(pdfreader.numPages):
-    page = pdfreader.getPage(pagenumber)
-    text += page.extractText()
-
-pdfobj.close()
-
-print(text)
-
-engine = pyttsx3.init()
-engine.say(text)
-engine.runAndWait()'''
+ '''
 
